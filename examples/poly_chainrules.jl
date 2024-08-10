2d07f0fc91bf0f3e0bd2638f46a2fd05d7a2f404
Lior Sinai <sinailior@gmail.com>
2024-08-10 12:20:41 +0200

    refactor: align rrule with output of pullback and minor edits

--- examples/poly_chainrules.jl
+++ examples/poly_chainrules.jl
@@ -1,6 +1,7 @@
 using Plots
 using Printf
 using LinearAlgebra
+using StatsBase
 
 # using Revise
 using MicroGrad
@@ -14,8 +15,22 @@ end
 
 (m::Polynomial)(x) = evalpoly(x, m.weights)
 (m::Polynomial)(x::AbstractVector) = map(x -> evalpoly(x, m.weights), x)
-rrule(m::Polynomial, x) = rrule(evalpoly, x, m.weights)
-rrule(m::Polynomial, x::AbstractVector) = pullback(map, m, x)
+function rrule(m::Polynomial, x)
+    y, back_poly = rrule(evalpoly, x, m.weights)
+    function back_Polynomial(Δ)
+        Δevalpoly, Δx, Δweights = back_poly(Δ)
+        ((;weights=Δweights), Δx)
+    end
+    y, back_Polynomial
+end
+function rrule(m::Polynomial, x::AbstractVector)
+    y, back_map = pullback(map, m, x)
+    function back_Polynomial(Δ)
+        Δmap, Δm, Δx = back_map(Δ)
+        (Δm, Δx)
+    end
+    y, back_Polynomial
+end
 
 # https://towardsdatascience.com/polynomial-regression-gradient-descent-from-scratch-279db2936fe9
 
@@ -23,18 +38,19 @@ function gradient_descent_poly!(coeffs::AbstractVector, xs::AbstractVector, ys::
     learning_rate::AbstractFloat=0.1, max_iters::Integer=100)
     history = Float64[]
     for i in 1:max_iters
-        loss_total = 0.0
-        for (x, y) in zip(xs, ys)
-            y_est, back = rrule(evalpoly, x, coeffs)
-            Δf, Δx, Δp = back(1.0)
-            error = (y - y_est)^2
-            Δerror = -2 * (y - y_est)
-            loss_total += error
-            coeffs .-= learning_rate .* Δp * Δerror
-        end
+        # forward
+        ys_and_backs = map(x->rrule(evalpoly, x, coeffs), xs)
+        ŷ = map(first, ys_and_backs)
+        loss_iter, back_loss = rrule(mse, ŷ, ys)
+        # reverse
+        Δloss, Δŷ, Δy = back_loss(1.0)
+        ∂f_and_∂x_zipped = map(((_, pb), δ) -> pb(δ), ys_and_backs, Δŷ)
+        Δcoeffs_unzipped = map(Δ->Δ[3], ∂f_and_∂x_zipped) # Δ[i] = (Δevalpoly, Δx, Δcoeffs)
+        Δcoeffs = reduce(+, Δcoeffs_unzipped)
+        # update
+        coeffs .-= learning_rate .* Δcoeffs
         # history
-        mean_loss = loss_total / length(xs)
-        push!(history, mean_loss)
+        push!(history, loss_iter)
     end
     history
 end
@@ -47,11 +63,10 @@ function gradient_descent!(model, xs::AbstractVector, ys::AbstractVector;
         ŷ, back_model = rrule(model, xs)
         loss_iter, back_loss = rrule(mse, ŷ, ys)
         # reverse
-        Δf, Δŷ, Δy = back_loss(1.0)
-        Δmap, Δf, Δx, Δweights = back_model(Δŷ)
+        Δmse, Δŷ, Δy = back_loss(1.0)
+        Δmodel, Δx = back_model(Δŷ)
         # update
-        cum_grad = reduce(+, Δweights)
-        model.weights .-= learning_rate .* cum_grad
+        model.weights .-= learning_rate .* Δmodel.weights
         # history
         push!(losses, loss_iter)  
     end
@@ -73,17 +88,42 @@ function solve_poly_linear(order::Int, xs::AbstractVector, ys::AbstractVector, )
     coeffs
 end
 
+function make_polynomial_label(coeffs::AbstractVector)
+    parts = [@sprintf("%.1fx^%d", c, i) for (i, c) in enumerate(coeffs[2:end])]
+    label = @sprintf "y=%.1f" coeffs[1]
+    for x_str in parts
+        if startswith(x_str, "-")
+            label *= x_str
+        else
+            label *= "+" * x_str
+        end
+    end
+    label
+end
+
+## Data
 target_weights = [15.0, -2.1, 13.9, 1.5]
+noise_factor = 0.2
 xs = (rand(100) .- 0.5) .* 10
 ys = map(x -> evalpoly(x, target_weights), xs)
-ys .+= randn(length(ys)) .* abs.(ys)/maximum(ys) * 100
-scatter(xs, ys)
+scale_factor = mean(abs.(ys))
+ys .+= randn(length(ys)) * scale_factor * noise_factor
+
+scatter(xs, ys, label="", size=(900, 600), tickfontsize=14)
+label = make_polynomial_label(target_weights)
+x_target = -5:0.01:5
+y_target = map(x -> evalpoly(x, target_weights), x_target)
+plot!(x_target, y_target, label=label, linewidth=2, legendfontsize=14)
+
+## Models
 
 weights = rand(4)
 model = Polynomial(copy(weights))
 coeffs_lin = solve_poly_linear(3, xs, ys)
 history1 = gradient_descent_poly!(weights, xs, ys; learning_rate=1e-5, max_iters=10_000)
 history2 = gradient_descent!(model, xs, ys; learning_rate=1e-5, max_iters=10_000)
+
+## Compare
 
 ys1 = map(x -> evalpoly(x, weights), xs)
 ys2 = model(xs)
@@ -93,12 +133,11 @@ for (label, ys_est) in [("linear", ys_lin), ("descent poly", ys1), ("descent mod
     @printf "%-14s: %.4f\n" label e
 end
 
-x_model = -5:0.01:5
-y_lin = map(x -> evalpoly(x, coeffs_lin), x_model)
-y_model1 = map(x -> evalpoly(x, weights), x_model)
-y_model2 = model(x_model)
+y_lin = map(x -> evalpoly(x, coeffs_lin), x_target)
+y_model1 = map(x -> evalpoly(x, weights), x_target)
+y_model2 = model(x_target)
 
-scatter(xs, ys)
-plot!(x_model, y_model1)
-plot!(x_model, y_model2)
-plot!(x_model, y_lin)
+scatter(xs, ys, label="")
+plot!(x_target, y_model1, label="gradient_descent_poly!")
+plot!(x_target, y_model2, label="gradient_descent!")
+plot!(x_target, y_lin, label="linear")
